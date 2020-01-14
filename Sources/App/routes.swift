39852473f9f284219de1200c56b7926bf2957ef1
Fabrício Masiero <masiero572@gmail.com>
2020-01-14 19:13:45 -0300

    update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "Vamos demitir a jay hoje mesmo?"
     }
     
     // Basic "Hello, world!" example
