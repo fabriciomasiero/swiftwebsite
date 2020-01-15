8484f461679778b8b33799ba6d1b0d12c30a8948
Fabrício Masiero <masiero572@gmail.com>
2020-01-15 13:50:37 -0300

    Adding view controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,18 +3,21 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "Website feito em SWIFT! SWIFT ROCKS!"
-    }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+    let websiteViewController = WebsiteController()
+    try router.register(collection: websiteViewController)
+//    router.get { req in
+//        return "Website feito em SWIFT! SWIFT ROCKS!"
+//    }
+//    
+//    // Basic "Hello, world!" example
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
 
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
